02320071ed00c19479f99b0ad363d0140579db89
Ana Vlaicu <ana.vlaicu@student.upt.ro>
2026-03-22 01:15:55 +0100

    Lab2

--- app/src/main/java/com/example/anavlaicuandoridpart1/MainActivity.kt
+++ app/src/main/java/com/example/anavlaicuandoridpart1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.anavlaicuandoridpart1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +14,17 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,7 +34,53 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTexts()
+           // MyTexts()
+            val ctx = LocalContext.current
+            var txt : MutableState<String> = remember {
+                mutableStateOf("This text will be changed")
+            }
+            var isButtonClicked = false
+            var tfValue : MutableState<String> = remember { mutableStateOf("") }
+            Column {
+                Button(onClick = {
+                    Log.i("myapp", "Entry from my application")
+                }) {
+                    Text(text = "Save to LogCat")
+                }
+
+                Button(onClick = {
+                    Toast.makeText(ctx, "Hello There!", LENGTH_LONG).show()
+                }) {
+                    Text(text = "Show Toast")
+                }
+
+                Button(onClick = {
+                    txt.value = "My New Text"
+                    Log.i("myapp",txt.value)
+                }){
+                    Text(text = "Change the text")
+                }
+
+                Text(
+                    text = txt.value,
+                    fontSize = 20.sp
+                )
+
+                Button(onClick = {
+                    isButtonClicked = true
+                }) {
+                    Text(text = "Show Up Colored Text")
+                }
+
+                if (isButtonClicked)
+                {
+                    MyTexts()
+                }
+
+                TextField(value = tfValue.value, onValueChange = {
+                    tfValue.value = it
+                })
+            }
         }
     }
 }
